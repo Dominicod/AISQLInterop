5abb453eff6c6321fcbd92537c2c4ccba01a260f
dominicod <dominicodonnell99@gmail.com>
2025-01-24 16:39:54 -0600

    Feat: Begin process of getting DB connected

--- init.py
+++ init.py
@@ -1,14 +1,57 @@
 import argparse
+import json
+import os
+
+from langchain_community.utilities import SQLDatabase
+from langchain_openai import ChatOpenAI
 
 
+# Builds and runs the SQL agent
 def run_sql_agent(prompt):
-    print(prompt)
+    llm = ChatOpenAI(model="gpt-4o-mini")
+    db = SQLDatabase.from_uri(f"mssql+pyodbc://{os.environ['DB_CONNECTION_STRING']}")
 
 
+#    toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+#    tools = toolkit.get_tools()
+
+#    prompt_template = hub.pull("langchain-ai/sql-agent-system-prompt:31156d5f")
+#    assert len(prompt_template.messages) == 1
+
+#    system_message = """System: You are an agent designed to interact with a SQL database filled with various account and tax data. Your name is ShankDuck.
+# Given an input question, create a syntactically correct SQLServer query to run, then look at the results of the query and return the answer.
+# You can order the results by a relevant column to return the most interesting examples in the database.
+# Never query for all the columns from a specific table, only ask for the relevant columns given the question.
+# You have access to tools for interacting with the database.
+# Only use the below tools. Only use the information returned by the below tools to construct your final answer.
+# You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
+
+# DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
+
+
+# To start you should ALWAYS look at the tables in the database to see what you can query.
+# Do NOT skip this step.
+# Then you should query the schema of the most relevant tables."""
+
+
+# Assign environment variables
+def _set_env(key: str, value: str):
+    if key not in os.environ:
+        os.environ[key] = value
+
+
+## Parse the command line arguments and run the agent
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--prompt", required=True)
     args = parser.parse_args()
+
+    # Load the secrets from the JSON file and set them as environment variables
+    with open("secrets.json", "r") as f:
+        secrets = json.load(f)
+        for key, value in secrets.items():
+            _set_env(key, value)
+
     run_sql_agent(args.prompt)
 
 
