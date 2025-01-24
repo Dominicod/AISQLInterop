47419f28827439b0e8baa310897ee8159c5b514d
dominicod <dominicodonnell99@gmail.com>
2025-01-24 15:03:14 -0600

    Feat: Setup basic skeleton

--- init.py
+++ init.py
@@ -1,1 +1,16 @@
-msg = "test"
+import argparse
+
+
+def run_sql_agent(prompt):
+    print(prompt)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--prompt", required=True)
+    args = parser.parse_args()
+    run_sql_agent(args.prompt)
+
+
+if __name__ == "__main__":
+    main()
