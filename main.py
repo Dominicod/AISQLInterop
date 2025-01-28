2e572aa69231e3e9932f658de8f2ae25ab330a79
dominicod <dominicodonnell99@gmail.com>
2025-01-28 13:00:39 -0600

    Feat: It's alive

--- main.py
+++ main.py
@@ -5,25 +5,26 @@ import os
 from langchain import hub
 from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 from langchain_community.utilities import SQLDatabase
+from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
 from langgraph.prebuilt import create_react_agent
 from sqlalchemy import create_engine
 
 
-def run_sql_agent(prompt: str):
-    llm = ChatOllama(model="mistral")
+def run_sql_agent(prompt: HumanMessage):
+    llm = ChatOllama(model="llama3.1", temperature=0)
+    prompt_template = hub.pull("langchain-ai/sql-agent-system-prompt")
+    system_message = prompt_template.format(dialect="SQLServer", top_k=5)
+
     db = init_sql_database()
     toolkit = SQLDatabaseToolkit(db=db, llm=llm)
-
-    prompt_template = hub.pull("langchain-ai/sql-agent-system-prompt")
-    assert len(prompt_template.messages) == 1
-    system_message = prompt_template.format(dialect="SQLServer", top_k=5)
 
     agent_executor = create_react_agent(
         llm, toolkit.get_tools(), state_modifier=system_message
     )
+
     events = agent_executor.stream(
-        {"messages": [("user", prompt)]},
+        {"messages": prompt},
         stream_mode="values",
     )
     for event in events:
@@ -56,7 +57,8 @@ def main():
         for key, value in secrets.items():
             _set_env(key, value)
 
-    run_sql_agent(args.prompt)
+    prompt = HumanMessage(args.prompt)
+    run_sql_agent(prompt)
 
 
 if __name__ == "__main__":
